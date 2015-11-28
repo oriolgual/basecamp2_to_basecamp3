84c81d30e1622d390d859d8a25a50147f1bd0ccd
Oriol Gual <oriol.gual@gmail.com>
2015-11-28 17:55:08 +0100

    Even better, now with multiple import

--- migrate_todos.rb
+++ migrate_todos.rb
@@ -34,8 +34,8 @@ def create_todo_list(todo_list_title, todo_list_items, basecamp_3_project_url)
 
     if todo.comments.any?
       session.click_link todo.content
-      todo.comments.each do |comment|
-        content = comment.content
+      todo.comments.select(&:content).each do |comment|
+        content = comment.content.to_s
         content += '<br>'
         content += "Originally posted by #{comment.creator.name} on #{Time.parse(comment.created_at).strftime("%Y-%m-%d %H:%M")}"
 
@@ -52,41 +52,47 @@ end
 # 2. Click on rename
 # 3. Copy the value
 # 4. Close
-default_firefox_profile_name = ARGV[0]
+default_firefox_profile_name, username, password, basecamp_3_project_url = ARGV.take(4)
+urls = ARGV[4, ARGV.length]
 
 Capybara.register_driver :custom_selenium do |app|
   Capybara::Selenium::Driver.new(app, :browser => :firefox, :profile => default_firefox_profile_name)
 end
 Capybara.default_driver = :custom_selenium
 
-account_id, project_id, todo_list_id = ARGV[3].split('/').map(&:to_i).select {|i| i > 0}
-Bcx.configure do |config|
-  config.account = account_id
-  config.user_agent = 'Basecamp 2, to Basecamp 3 todo migrator'
+urls.each do |url|
+  account_id, project_id, todo_list_id = url.split('/').map(&:to_i).select {|i| i > 0}
+  Bcx.configure do |config|
+    config.account = account_id
+    config.user_agent = 'Basecamp 2, to Basecamp 3 todo migrator'
+  end
+
+  usage = "ruby migrate_todo.rb firefox_profile_id basecamp_2_username basecamp_2_password basecamp_3_project_todos_url basecamp_2_todolist_urls"
+
+  raise "Missing Firefox profile id. Usage: #{usage}" unless default_firefox_profile_name
+  raise "Missing Basecamp 2 account. Usage: #{usage}" unless account_id
+  raise "Missing Basecamp 2 username. Usage: #{usage}" unless username
+  raise "Missing Basecamp 2 password. Usage: #{usage}" unless password
+  raise "Missing Basecamp 2 project_id. Usage: #{usage}" unless project_id
+  raise "Missing Basecamp 2 todo_list_id. Usage: #{usage}" unless todo_list_id
+  raise "Missing Basecamp 3 project url. Usage: #{usage}" unless basecamp_3_project_url
+
+  puts "Fetching data from Basecamp 2 for #{url}"
+  client = Bcx::Client::HTTP.new(login: username, password: password)
+
+  todos = client.projects(project_id).todolists(todo_list_id).todos!
+  todo_list_title = todos.first.todolist.name
+  todo_list_items = todos.sort_by(&:position).reject(&:completed).map do |todo|
+    client.projects(project_id).todolists(todo_list_id).todos!(todo.id)
+  end
+
+  begin
+    create_todo_list(todo_list_title, todo_list_items, basecamp_3_project_url)
+  rescue Exception => e
+    puts e.message
+    byebug
+  end
+
+  puts "Finished importing #{url}"
 end
-
-username = ARGV[1]
-password = ARGV[2]
-basecamp_3_project_url = ARGV[4]
-usage = "ruby migrate_todo.rb firefox_profile_id basecamp_2_username basecamp_2_password basecamp_2_todolist_url basecamp_3_project_todos_url"
-
-raise "Missing Firefox profile id. Usage: #{usage}" unless default_firefox_profile_name
-raise "Missing Basecamp 2 account. Usage: #{usage}" unless account_id
-raise "Missing Basecamp 2 username. Usage: #{usage}" unless username
-raise "Missing Basecamp 2 password. Usage: #{usage}" unless password
-raise "Missing Basecamp 2 project_id. Usage: #{usage}" unless project_id
-raise "Missing Basecamp 2 todo_list_id. Usage: #{usage}" unless todo_list_id
-raise "Missing Basecamp 3 project url. Usage: #{usage}" unless basecamp_3_project_url
-
-puts "Fetching data from Basecamp 2"
-client = Bcx::Client::HTTP.new(login: username, password: password)
-
-todos = client.projects(project_id).todolists(todo_list_id).todos!
-todo_list_title = todos.first.todolist.name
-todo_list_items = todos.sort_by(&:position).map do |todo|
-  client.projects(project_id).todolists(todo_list_id).todos!(todo.id)
-end
-
-create_todo_list(todo_list_title, todo_list_items, basecamp_3_project_url)
-
 puts 'Finished!'
